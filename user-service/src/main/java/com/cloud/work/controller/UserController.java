8b1c2e9c4f3946342e7b954f7de07f77181511a6
duyphuong2205 <phuongvdd@seatechit.com.vn>
2026-01-21 15:49:34 +0700

    config open api for user service

--- user-service/src/main/java/com/cloud/work/controller/UserController.java
+++ user-service/src/main/java/com/cloud/work/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
